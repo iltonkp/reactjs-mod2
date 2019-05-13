0ad01ac4c1e5937d0f06e7ebf0aa1e7dc432b983
iltonkp <iltonk.si@gmail.com>
2019-05-13 15:10:15 -0300

    GoReact - mod2 - Estilizando  o componente ComparaList

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -1,28 +1,78 @@
 import React from 'react';
 
-const ComapareList = () => (
-  <div>
-    <header>
-      <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
-      <strong>react</strong>
-      <small>Facebook</small>
-    </header>
+import { Container, Repository } from './style';
 
-    <ul>
-      <li>
-        95,819 <small>stars</small>
-      </li>
-      <li>
-        95,819 <small>forks</small>
-      </li>
-      <li>
-        95,819 <small>issues</small>
-      </li>
-      <li>
-        95,819 <small>last commit</small>
-      </li>
-    </ul>
-  </div>
+const ComapareList = () => (
+  <Container>
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
+        <strong>react</strong>
+        <small>Facebook</small>
+      </header>
+
+      <ul>
+        <li>
+          95,819 <small>stars</small>
+        </li>
+        <li>
+          95,819 <small>forks</small>
+        </li>
+        <li>
+          95,819 <small>issues</small>
+        </li>
+        <li>
+          95,819 <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
+        <strong>react</strong>
+        <small>Facebook</small>
+      </header>
+
+      <ul>
+        <li>
+          95,819 <small>stars</small>
+        </li>
+        <li>
+          95,819 <small>forks</small>
+        </li>
+        <li>
+          95,819 <small>issues</small>
+        </li>
+        <li>
+          95,819 <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+
+    <Repository>
+      <header>
+        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
+        <strong>react</strong>
+        <small>Facebook</small>
+      </header>
+
+      <ul>
+        <li>
+          95,819 <small>stars</small>
+        </li>
+        <li>
+          95,819 <small>forks</small>
+        </li>
+        <li>
+          95,819 <small>issues</small>
+        </li>
+        <li>
+          95,819 <small>last commit</small>
+        </li>
+      </ul>
+    </Repository>
+  </Container>
 );
 
 export default ComapareList;
