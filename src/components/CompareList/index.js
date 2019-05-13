b2c8f694be3d111a58f537aef6bdd689dd63f98d
iltonkp <iltonk.si@gmail.com>
2019-05-13 15:47:27 -0300

    GoReact - mod2 - Enviando repositórios com props

--- src/components/CompareList/index.js
+++ src/components/CompareList/index.js
@@ -2,76 +2,32 @@ import React from 'react';
 
 import { Container, Repository } from './style';
 
-const ComapareList = () => (
+const ComapareList = ({ repositories }) => (
   <Container>
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
-        <strong>react</strong>
-        <small>Facebook</small>
-      </header>
+    {repositories.map(repository => (
+      <Repository>
+        <header>
+          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+          <strong>{repository.name}</strong>
+          <small>{repository.owner.login}</small>
+        </header>
 
-      <ul>
-        <li>
-          95,819 <small>stars</small>
-        </li>
-        <li>
-          95,819 <small>forks</small>
-        </li>
-        <li>
-          95,819 <small>issues</small>
-        </li>
-        <li>
-          95,819 <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
-        <strong>react</strong>
-        <small>Facebook</small>
-      </header>
-
-      <ul>
-        <li>
-          95,819 <small>stars</small>
-        </li>
-        <li>
-          95,819 <small>forks</small>
-        </li>
-        <li>
-          95,819 <small>issues</small>
-        </li>
-        <li>
-          95,819 <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
-
-    <Repository>
-      <header>
-        <img src="https://avatars3.githubusercontent.com/u/69631?v=4" alt="Facebook" />
-        <strong>react</strong>
-        <small>Facebook</small>
-      </header>
-
-      <ul>
-        <li>
-          95,819 <small>stars</small>
-        </li>
-        <li>
-          95,819 <small>forks</small>
-        </li>
-        <li>
-          95,819 <small>issues</small>
-        </li>
-        <li>
-          95,819 <small>last commit</small>
-        </li>
-      </ul>
-    </Repository>
+        <ul>
+          <li>
+            {repository.stargazers_count} <small>stars</small>
+          </li>
+          <li>
+            {repository.forks_count} <small>forks</small>
+          </li>
+          <li>
+            {repository.open_issues_count} <small>issues</small>
+          </li>
+          <li>
+            {repository.pushed_at} <small>last commit</small>
+          </li>
+        </ul>
+      </Repository>
+    ))}
   </Container>
 );
 
